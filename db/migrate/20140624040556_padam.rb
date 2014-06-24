703eb6c573e7d124ec325ea9082028fff9c9aca8
Srikanth <srikanth@thoughworks.com>
2014-06-24 10:52:57 +0530

    fixed the db seed script

--- db/migrate/20140624040556_padam.rb
+++
@@ -1,4 +0,0 @@
-class Padam < ActiveRecord::Migration
-  execute("UPDATE padams SET name='परस्मैपदी' WHERE name='परस्मैपदम्';")
-  execute("UPDATE padams SET name='आत्मनेपदी' WHERE name='आत्मनेपदम्';")
-end
