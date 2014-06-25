b64ae8ac973316bed1a82c967240a02f6898cffd
Srikanth <srikanth@thoughworks.com>
2014-06-25 09:10:07 +0530

    removed id based migration

--- db/migrate/20140624152154_update_padams_using_id.rb
+++
@@ -1,6 +0,0 @@
-class UpdatePadamsUsingId < ActiveRecord::Migration
-  def change
-  end
-  execute("UPDATE padams SET name='परस्मैपदी' WHERE id=1;")
-  execute("UPDATE padams SET name='आत्मनेपदी'  WHERE id=2;")
-end
