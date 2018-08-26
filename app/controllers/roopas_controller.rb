3d9afc6e8d4528243498c282a2e086f49dc8006e
Srikanth <srikanths@sahajsoft.com>
2018-08-26 21:00:22 +0530

    fixed partial match

--- app/controllers/roopas_controller.rb
+++ app/controllers/roopas_controller.rb
@@ -12,18 +12,18 @@ class RoopasController < ApplicationController
       @roopa = Roopa.includes(:dhatu, :purusha, :vachanam, :lakaras).where(
           :name => [name.strip, name_with_colon.strip, name_with_unicode.strip])
 
-      # if @roopa.empty?
-      #   candidates = Roopa.includes(:dhatu, :purusha, :vachanam, :lakaras)
-      #                    .where("name LIKE ? OR name LIKE ? OR name LIKE ?",
-      #                           "%#{name}%", "%#{name_with_colon}%", "%#{name_with_unicode}%")
-      #   unless candidates.empty?
-      #     candidates.each do |r|
-      #       r.name.split(/\//).each { |s|
-      #         @roopa << r if s.strip == name.strip
-      #       }
-      #     end
-      #   end
-      # end
+      if @roopa.empty?
+        candidates = Roopa.includes(:dhatu, :purusha, :vachanam, :lakaras)
+                         .where("name LIKE ? OR name LIKE ? OR name LIKE ?",
+                                "%#{name}/%", "%#{name_with_colon}/%", "%#{name_with_unicode}/%")
+        unless candidates.empty?
+          candidates.each do |r|
+            r.name.split(/\//).each { |s|
+              @roopa << r if s.strip == name.strip
+            }
+          end
+        end
+      end
       @roopa.each do |r|
         @roopa_name_map[r.name]=name unless r.name.include? "/"
       end
